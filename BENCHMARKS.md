c363709ca604e60fabad164377730cdef845995b
Benno Meysmans <benno_njord@hotmail.com>
2020-06-02 18:18:40 +0200

    Uncomment benchmarks

--- BENCHMARKS.md
+++ BENCHMARKS.md
@@ -2,7 +2,7 @@ Pre optimizations:
 
 type: 0.011828 ms
 record: 0.00934 ms
-array: 80.8 ms / 1.000.000 items
-union: 0.011488 ms
+array: 73.3 ms / 1.000.000 items
+union: 0.006896 ms
 test:good: 0.016604 ms
-test:bad 0.017968
+test:bad 0.016082 ms
--- benchmarks.test.ts
+++ benchmarks.test.ts
@@ -11,199 +11,199 @@ import { partial } from "./lib/decoders/partial.ts";
 import { intersection } from "./lib/decoders/intersection.ts";
 import { tuple } from "./lib/decoders/tuple.ts";
 
-// bench({
-//   name: "type",
-//   runs: 1000000,
-//   func: ({ stop, start }) => {
-//     const decoder = type({
-//       firstName: string(),
-//       lastName: string(),
-//       age: number(),
-//       meta: partial({
-//         contact: boolean(),
-//         interests: array(
-//           union([literal("OFFERS"), literal("BLOG_POSTS"), literal("NEWS")])
-//         ),
-//       }),
-//       addresses: array(
-//         type({
-//           street: string(),
-//           city: string(),
-//           zipCode: number(),
-//         })
-//       ),
-//     });
+bench({
+  name: "type",
+  runs: 1000000,
+  func: ({ stop, start }) => {
+    const decoder = type({
+      firstName: string(),
+      lastName: string(),
+      age: number(),
+      meta: partial({
+        contact: boolean(),
+        interests: array(
+          union([literal("OFFERS"), literal("BLOG_POSTS"), literal("NEWS")])
+        ),
+      }),
+      addresses: array(
+        type({
+          street: string(),
+          city: string(),
+          zipCode: number(),
+        })
+      ),
+    });
 
-//     start();
-//     decoder.decode({
-//       firstName: "Ronald",
-//       lastName: "McDonald",
-//       age: 80,
-//       meta: {
-//         interests: ["OFFERS", "NEWS"],
-//       },
-//       addresses: [
-//         {
-//           street: "High Street One",
-//           city: "Someplace",
-//           zipCode: 2000,
-//         },
-//       ],
-//     });
-//     stop();
-//   },
-// });
+    start();
+    decoder.decode({
+      firstName: "Ronald",
+      lastName: "McDonald",
+      age: 80,
+      meta: {
+        interests: ["OFFERS", "NEWS"],
+      },
+      addresses: [
+        {
+          street: "High Street One",
+          city: "Someplace",
+          zipCode: 2000,
+        },
+      ],
+    });
+    stop();
+  },
+});
 
-// bench({
-//   name: "record",
-//   runs: 1000000,
-//   func: ({ stop, start }) => {
-//     const decoder = record(string());
+bench({
+  name: "record",
+  runs: 1000000,
+  func: ({ stop, start }) => {
+    const decoder = record(string());
 
-//     start();
-//     decoder.decode({ prop1: "Hey", prop2: "Buds", prop3: 123 });
-//     stop();
-//   },
-// });
+    start();
+    decoder.decode({ prop1: "Hey", prop2: "Buds", prop3: 123 });
+    stop();
+  },
+});
 
-// bench({
-//   name: "array",
-//   runs: 10,
-//   func: ({ start, stop }) => {
-//     const decoder = array(string());
-//     const testData: string[] = [];
-//     for (let i = 0; i < 1000000; i++) {
-//       testData.push(`Number: ${i}`);
-//     }
+bench({
+  name: "array",
+  runs: 10,
+  func: ({ start, stop }) => {
+    const decoder = array(string());
+    const testData: string[] = [];
+    for (let i = 0; i < 1000000; i++) {
+      testData.push(`Number: ${i}`);
+    }
 
-//     start();
-//     decoder.decode(testData);
-//     stop();
-//   },
-// });
+    start();
+    decoder.decode(testData);
+    stop();
+  },
+});
 
-// bench({
-//   name: "union",
-//   runs: 1000000,
-//   func: ({ stop, start }) => {
-//     const decoder = union([
-//       literal(200),
-//       literal(201),
-//       literal(202),
-//       literal(204),
-//       literal(400),
-//       literal(401),
-//       literal(403),
-//       literal(404),
-//       literal(500),
-//     ]);
+bench({
+  name: "union",
+  runs: 1000000,
+  func: ({ stop, start }) => {
+    const decoder = union([
+      literal(200),
+      literal(201),
+      literal(202),
+      literal(204),
+      literal(400),
+      literal(401),
+      literal(403),
+      literal(404),
+      literal(500),
+    ]);
 
-//     start();
-//     decoder.decode("200");
-//     stop();
-//   },
-// });
+    start();
+    decoder.decode("200");
+    stop();
+  },
+});
 
-// const Vector = tuple([number(), number(), number()]);
+const Vector = tuple([number(), number(), number()]);
 
-// const Asteroid = type({
-//   type: literal("asteroid"),
-//   location: Vector,
-//   mass: number(),
-// });
+const Asteroid = type({
+  type: literal("asteroid"),
+  location: Vector,
+  mass: number(),
+});
 
-// const Planet = type({
-//   type: literal("planet"),
-//   location: Vector,
-//   mass: number(),
-//   population: number(),
-//   habitable: boolean(),
-// });
+const Planet = type({
+  type: literal("planet"),
+  location: Vector,
+  mass: number(),
+  population: number(),
+  habitable: boolean(),
+});
 
-// const Rank = union([
-//   literal("captain"),
-//   literal("first mate"),
-//   literal("officer"),
-//   literal("ensign"),
-// ]);
+const Rank = union([
+  literal("captain"),
+  literal("first mate"),
+  literal("officer"),
+  literal("ensign"),
+]);
 
-// const CrewMember = type({
-//   name: string(),
-//   age: number(),
-//   rank: Rank,
-//   home: Planet,
-// });
+const CrewMember = type({
+  name: string(),
+  age: number(),
+  rank: Rank,
+  home: Planet,
+});
 
-// const Ship = type({
-//   type: literal("ship"),
-//   location: Vector,
-//   mass: number(),
-//   name: string(),
-//   crew: array(CrewMember),
-// });
+const Ship = type({
+  type: literal("ship"),
+  location: Vector,
+  mass: number(),
+  name: string(),
+  crew: array(CrewMember),
+});
 
-// const decoder = union([Asteroid, Planet, Ship]);
+const decoder = union([Asteroid, Planet, Ship]);
 
-// const good = {
-//   type: "ship",
-//   location: [1, 2, 3],
-//   mass: 4,
-//   name: "foo",
-//   crew: [
-//     {
-//       name: "bar",
-//       age: 44,
-//       rank: "captain",
-//       home: {
-//         type: "planet",
-//         location: [5, 6, 7],
-//         mass: 8,
-//         population: 1000,
-//         habitable: true,
-//       },
-//     },
-//   ],
-// };
+const good = {
+  type: "ship",
+  location: [1, 2, 3],
+  mass: 4,
+  name: "foo",
+  crew: [
+    {
+      name: "bar",
+      age: 44,
+      rank: "captain",
+      home: {
+        type: "planet",
+        location: [5, 6, 7],
+        mass: 8,
+        population: 1000,
+        habitable: true,
+      },
+    },
+  ],
+};
 
-// const bad = {
-//   type: "ship",
-//   location: [1, 2, "a"],
-//   mass: 4,
-//   name: "foo",
-//   crew: [
-//     {
-//       name: "bar",
-//       age: 44,
-//       rank: "captain",
-//       home: {
-//         type: "planet",
-//         location: [5, 6, 7],
-//         mass: 8,
-//         population: "a",
-//         habitable: true,
-//       },
-//     },
-//   ],
-// };
+const bad = {
+  type: "ship",
+  location: [1, 2, "a"],
+  mass: 4,
+  name: "foo",
+  crew: [
+    {
+      name: "bar",
+      age: 44,
+      rank: "captain",
+      home: {
+        type: "planet",
+        location: [5, 6, 7],
+        mass: 8,
+        population: "a",
+        habitable: true,
+      },
+    },
+  ],
+};
 
-// bench({
-//   name: "test:good",
-//   runs: 1000000,
-//   func: ({ start, stop }) => {
-//     start();
-//     decoder.decode(good);
-//     stop();
-//   },
-// });
+bench({
+  name: "test:good",
+  runs: 1000000,
+  func: ({ start, stop }) => {
+    start();
+    decoder.decode(good);
+    stop();
+  },
+});
 
-// bench({
-//   name: "test:bad",
-//   runs: 1000000,
-//   func: ({ start, stop }) => {
-//     start();
-//     decoder.decode(bad);
-//     stop();
-//   },
-// });
+bench({
+  name: "test:bad",
+  runs: 1000000,
+  func: ({ start, stop }) => {
+    start();
+    decoder.decode(bad);
+    stop();
+  },
+});
 
 runBenchmarks();
